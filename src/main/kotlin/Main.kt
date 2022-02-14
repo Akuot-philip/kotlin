9ed74635aebce6b2aa6c78da6418b3ea2b02ee12
Akuot-philip <akuotphilipp@gmail.com>
2022-02-14 12:12:13 +0300

    y first kotlin project

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+    var name = arrayOf("Saido", "AKuot", "Lucy","Pj","Philip")
+    var age = arrayOf(20,30,40,50,60)
+    var phonenumber = arrayOf(25492345087,25422864325,25412345678,25423456789)
+    var weight = arrayOf("50kg","60kg","70kg","80kg","90kg")
+    var citizen = arrayOf("true","false","true","false","true")
+
+    //first student
+    println(name[0])
+    println(age[0])
+    println(phonenumber[0])
+    println(weight[0])
+    println(citizen[0])
+
+    //second student
+    println(name[1])
+    println(age[1])
+    println(phonenumber[1])
+    println(weight[1])
+    println(citizen[1])
+
+    //second student
+    println(name[2])
+    println(age[2])
+    println(phonenumber[2])
+    println(weight[2])
+    println(citizen[2])
+
+    //third student
+    println(name[3])
+    println(age[3])
+    println(phonenumber[3])
+    println(weight[3])
+    println(citizen[3])
+
+
+
+
+
+}
